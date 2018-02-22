c949bf728bf032148954191030f8c0848d75c692
Jason Giang <jagiang3@s.sfusd.edu>
2018-02-22 01:16:46 +0000

    gif

--- js/script.js
+++ js/script.js
@@ -14,16 +14,37 @@ $(document).ready(function(){
   function giphyURLWithSearchTerm(searchTerm) {
       // write a function that will return a url for the giphy API with
       // the searchTerm provided in the parameters
+    var firstPart = "api.giphy.com/v1/stickers/search?q=";
+    var term = "dog";
+    var secondPart = "&api_key=dc6zaTOxFJmzC";
+    var locaTerm = firstPart + term + secondPart;
+    console.log(locaTerm);
   }
 
   function appendImageToGallery(srcURL) {
       // write a function that will append an <img> to the div with class="gallery"
       // using the URL provided in the parameters
+      var url = "<img src="+giphyURLWithSearchTerm+">";
+      $("body").append(url);
   }
 
   function callGiphyAPIWithSearchTerm(searchTerm) {
       // use $.ajax to call the giphy api with the given search term from the parameters.
       // get the first image url from the ajax response
       // use appendImageToGallery to put the image onto the screen
-  }
+      $.ajax({
+      url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
+      method: "GET",
+      success: function(response) {
+           // Log the whole response to the console
+      console.log();
+           // Log the first image of the data to the console
+           
+           // Log the "type" property of the first image object to the console
+           
+           // Log the "title" property of the first image object to the console
+      },
+    }
 });
+
+callGiphyAPIWithSearchTerm("dog");
